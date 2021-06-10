94113d3531a05ab89050d78a369812726719151c
Petter Tondel <petter.toendel@outlook.com>
2021-06-10 17:36:58 +0200

    Slettet prosjekt. Pullet ned og fungerer hos meg

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,8 +15,10 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			System.out.println("Din applikasjon fungerer");
 		} catch(Exception e) {
 			e.printStackTrace();
+			System.out.println("Dette gikk dårlig. Prøv igjen");
 		}
 	}
 	
