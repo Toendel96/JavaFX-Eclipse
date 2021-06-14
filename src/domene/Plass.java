d7f45f3c5e6eecffc857f3875c35eb1bb450260f
Petter Tondel <petter.toendel@outlook.com>
2021-06-14 12:01:20 +0200

    Fikset Plass

--- src/domene/Plass.java
+++ src/domene/Plass.java
@@ -1,5 +1,44 @@
 package domene;
 
 public class Plass {
+	private int radnr; 
+	private int setenr; 
+	private int kinosalnr;
+	
+	public Plass(int radnr, int setenr, int kinosalnr) {
+		super();
+		this.radnr = radnr;
+		this.setenr = setenr;
+		this.kinosalnr = kinosalnr;
+	}
+
+	public int getRadnr() {
+		return radnr;
+	}
+
+	public void setRadnr(int radnr) {
+		this.radnr = radnr;
+	}
+
+	public int getSetenr() {
+		return setenr;
+	}
+
+	public void setSetenr(int setenr) {
+		this.setenr = setenr;
+	}
+
+	public int getKinosalnr() {
+		return kinosalnr;
+	}
+
+	public void setKinosalnr(int kinosalnr) {
+		this.kinosalnr = kinosalnr;
+	}
+
+	@Override
+	public String toString() {
+		return "Plass [radnr=" + radnr + ", setenr=" + setenr + ", kinosalnr=" + kinosalnr + "]";
+	}
 
 }
