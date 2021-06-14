a4b50ffc3e3a053c9922fce83b027c7eddedb6d3
EndreTK <224883@usn.no>
2021-06-14 11:49:09 +0200

    Billett/Film

--- src/domene/Billett.java
+++ src/domene/Billett.java
@@ -2,4 +2,33 @@ package domene;
 
 public class Billett {
 
+	private String billettkode;
+	private int visningsnr;
+	private int erBetalt;
+	
+	public String getBillettkode() {
+		return billettkode;
+	}
+	public void setBillettkode(String billettkode) {
+		this.billettkode = billettkode;
+	}
+	public int getVisningsnr() {
+		return visningsnr;
+	}
+	public void setVisningsnr(int visningsnr) {
+		this.visningsnr = visningsnr;
+	}
+	public int getErBetalt() {
+		return erBetalt;
+	}
+	public void setErBetalt(int erBetalt) {
+		this.erBetalt = erBetalt;
+	}
+	
+	@Override
+	public String toString() {
+		return "Billett [billettkode=" + billettkode + ", visningsnr=" + visningsnr + ", erBetalt=" + erBetalt + "]";
+	}
+	
+	
 }
--- src/domene/Film.java
+++ src/domene/Film.java
@@ -1,5 +1,29 @@
 package domene;
 
 public class Film {
+	
+	private int filmnr;
+	private String filmnavn;
+	
+	
+	public int getFilmnr() {
+		return filmnr;
+	}
+	public void setFilmnr(int filmnr) {
+		this.filmnr = filmnr;
+	}
+	public String getFilmnavn() {
+		return filmnavn;
+	}
+	public void setFilmnavn(String filmnavn) {
+		this.filmnavn = filmnavn;
+	}
+	
+	@Override
+	public String toString() {
+		return "Film [filmnr=" + filmnr + ", filmnavn=" + filmnavn + "]";
+	}
+	
+	
 
 }
