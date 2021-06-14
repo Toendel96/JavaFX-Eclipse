8f8bc756a605bce420959c0785e379a1485f50d7
Andreas <ajhagman@gmail.com>
2021-06-14 11:44:08 +0200

    Atributter og piss til 2 klasser

--- src/domene/Kinosal.java
+++ src/domene/Kinosal.java
@@ -1,5 +1,32 @@
 package domene;
 
 public class Kinosal {
-
+	private int kinosalnr;
+	private String kinonavn;
+	private String kinosalnavn;
+	
+	public int getKinosalnr() {
+		return kinosalnr;
+	}
+	public void setKinosalnr(int kinosalnr) {
+		this.kinosalnr = kinosalnr;
+	}
+	public String getKinonavn() {
+		return kinonavn;
+	}
+	public void setKinonavn(String kinonavn) {
+		this.kinonavn = kinonavn;
+	}
+	public String getKinosalnavn() {
+		return kinosalnavn;
+	}
+	public void setKinosalnavn(String kinosalnavn) {
+		this.kinosalnavn = kinosalnavn;
+	}
+	
+	@Override
+	public String toString() {
+		return "Kinosal [kinosalnr=" + kinosalnr + ", kinonavn=" + kinonavn + ", kinosalnavn=" + kinosalnavn + "]";
+	}
+	
 }
