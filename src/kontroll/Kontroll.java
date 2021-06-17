25ca9bb4e979935959939883b6a46e191a8fb57b
Remi Iversen <remiiversen@Remis-MBP.home>
2021-06-17 09:22:24 +0200

    endringer

--- src/kontroll/Kontroll.java
+++ src/kontroll/Kontroll.java
@@ -397,8 +397,8 @@ public class Kontroll implements kontrollInterface {
 		String string2 = "";
 		
 		string2 = "Bestilling bekreftet\n";
-		string2 = string2 + "Billettene må hentes senest 30 minutter før forestillingen\n";
-		string2 = string2 + "Du må oppgi billettkoden når du kommer";
+		string2 = string2 + "Billettene mï¿½ hentes senest 30 minutter fï¿½r forestillingen\n";
+		string2 = string2 + "Du mï¿½ oppgi billettkoden nï¿½r du kommer";
 		
 		ok.setContentText(string2);
 		avbrutt.setContentText("Du fullforte ikke bestillingen");
@@ -696,7 +696,7 @@ public class Kontroll implements kontrollInterface {
 		for (Billett b:billett) {
 			for (Visning v: alleVisninger) {
 				if(b.getVisningsnr()==v.getVisningnr()) {
-					//Kall på metode som sjekker mot 30 min
+					//Kall pï¿½ metode som sjekker mot 30 min
 					if(sjekkOmDatoTidErFremtid(v.getStarttid(),v.getDato(), 30)) {
 						//Billetten er lengere enn 30min fram i tid
 					}else {	
@@ -769,21 +769,20 @@ public String getStatistikkFilm(String filmNr) {
 		String string = "";
 		String filmnr = filmNr;
 		int visningnr = 0;
-		int antallSett = 0;
-		String billettKode = null;
+		String antallSett = "0";
+		String billettKode = "0";
+		int slettet = 0;
 		
 		
 		string = string + " " + "Visningnr" + "         ";
-		string = string + " " + "Antall sett" + "         ";	
-		string = string + " " + "Prosent kinosal" + "         ";	
-		string = string + " " + "Bestilling slettet" + "\n";
+		string = string + " " + "Antall sett" + "         ";		
+		string = string + " " + "Bestillinger slettet" + "\n";
 		
 		for (Visning v : getAlleVisninger()) {
 			if (String.valueOf(v.getFilmnr()).equals(filmnr)) {
 			visningnr = v.getVisningnr();
-			string = string + "   " + visningnr+" ";
 			
-			}
+			
 			Date dato = v.getDato();
 			Time tid = v.getStarttid();
 			boolean status = sjekkOmDatoTidErFremtid(tid,dato,0);
@@ -794,21 +793,30 @@ public String getStatistikkFilm(String filmNr) {
 				if (b.getVisningsnr()==(visningnr)) {
 					if (b.getErBetalt()) {
 						billettKode = b.getBillettkode();
+						
+						for (Plassbillett pb : getPlassbillett()) {
+							if (String.valueOf(pb.getBillettkode()).equals(billettKode)) {
+								System.out.println(billettKode);
+								antallSett = String.valueOf(billettKode);
+							}
+							else {
+								antallSett="0";
+							}
+						} 
+						
 					}
 				}
+				
 			}
-			for (Plassbillett pb : getPlassbillett()) {
-				if (String.valueOf(pb.getBillettkode()).equals(billettKode)) {
-					antallSett++;
-				}
-				else {
-					antallSett=0;
-				}
+				
+			} 
+			string = string + "      " + visningnr+"                         ";
+			string = string + " " + antallSett + "                           ";
+			string = string + " " + slettet + "\n";
+			
 			}
-			}
-			//string = string + " " + visningnr + "  "; 
-			string = string + " " + antallSett + "\n";	
 		}
+		
 		
 								
 		return string;
@@ -954,7 +962,7 @@ public String getStatistikkKino(String kinosalNr) {
 				String visningsnr = innhold.nextToken();
 				ubillettlisteFrafil.add(new Billett(billettkode, Integer.parseInt(visningsnr),false));
 				linje = innfil.readLine();
-			} //løkke
+			} //lï¿½kke
 			for(Billett b:ubillettlisteFrafil) {
 			}
 			innfil.close();
